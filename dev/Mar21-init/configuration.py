e328ab5bb9733704fcaefad0273820c8665d717b
Haynes <chaynes32@gatech.edu>
2025-03-21 13:58:22 -0400

    config starter

---
+++ dev/Mar21-init/configuration.py
@@ -0,0 +1,14 @@
+import numpy as np
+import math as m
+from scipy import constants
+
+
+# ALL parameters, state equations, and other system / solver configuration info 
+
+
+## phyiscal constants
+mp = constants.m_p
+me = constants.m_e
+c = constants.c
+mu0 = constants.mu_0
+
