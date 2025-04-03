711ca713bc5601cf3c9553eabe9895a853ff7a8d
Neil Baker <bakerneil0@gmail.com>
2025-04-03 17:22:12 -0400

    neb refinement and restructuring of cmh rewrite; plots included

---
+++ 04-03-neb/Library.py
@@ -0,0 +1,120 @@
+import numpy as np
+import math as m
+
+from scipy import constants
+import matplotlib.pyplot as plt
+import configuration as c
+import os
+
+
+# minmod (see equation 2.8 and following discussion in Balbas et al)
+def minmod(alpha, wjm, wj, wjp):
+    if np.sign(wjm) == np.sign(wj) and np.sign(wj) == np.sign(wjp):
+        delminus = alpha * (wj - wjm)
+        delplus = alpha * (wjp - wj)
+        del0 = (1./2.) * (delminus + delplus)
+        # calculate minimums in the least efficient and most verbose way possible
+        minl = min(delminus,del0)
+        minp = min(delplus,del0)
+        return_min = min(minl,minp)
+        
+        return np.sign(wj) * return_min
+    
+    else:
+        return 0.
+
+# calculate f() for all quantities in a single vector (following Balbas explicitly)
+def calc_f(meshOBJ, Bx, gam):
+    fs = np.zeros_like(meshOBJ)
+    for i in range(meshOBJ.shape[1]):
+        fs[:,i] = calc_f_cell(meshOBJ[:,i], Bx, gam)
+    return fs
+
+# function to actually encode the MHD equations: calculating the argument of the divergence at each cell
+def calc_f_cell(cell, Bx, gam):
+    # unpack the values of u vector (see equation 4.1 in balbas)
+    rho, rho_ux, rho_uy, rho_uz, By, Bz, e = cell
+    # equivalent of our old "reconstruct" routine
+    ux = rho_ux / rho
+    uy = rho_uy / rho
+    uz = rho_uz / rho
+    B2 = Bx**2 + By**2 + Bz**2
+    v2 = ux**2 + uy**2 + uz**2
+    
+    # Energy equation
+    p = (gam-1.) * (e - ((1./2.) * rho * v2) - (1./2.) * B2)
+    pstar = p + (1./2.) * B2
+    
+    # calculate the equations of MHD in a vectorized manner
+    # following the expression blindly from Balbas itself, instead of worrying about units and physics
+    # see equation (4.2, 4.3)
+    f0 = rho_ux
+    f1 = (rho_ux**2 / rho) + pstar - Bx**2
+    f2 = (rho_ux * uy) - Bx * By
+    f3 = (rho_ux * uz) - Bx * Bz
+    f4 = (By * ux) - Bx * uy
+    f5 = (Bz * ux) - Bx * uz
+    f6 = (e + pstar) * ux - Bx * np.dot(np.array([Bx,By,Bz]),np.array([ux,uy,uz]))
+    
+    return np.array([f0, f1, f2, f3, f4, f5, f6])
+
+# plotting function
+def animate(meshOBJ, stagger_switch, tL, now):
+    """saves individual plots for each variable"""
+    
+    plot_dir = 'Plots ' + str(now)
+    # check if directories exist
+    if os.path.exists(plot_dir):
+        print(f"Saving new plots")
+    else:
+        os.mkdir(plot_dir)
+        os.mkdir(plot_dir+"/rho")
+        os.mkdir(plot_dir+"/u_x")
+        os.mkdir(plot_dir+"/u_y")
+        os.mkdir(plot_dir+"/u_z")
+        os.mkdir(plot_dir+"/B_x")
+        os.mkdir(plot_dir+"/B_y")
+        os.mkdir(plot_dir+"/energy")
+        os.mkdir(plot_dir+"/pressure")
+        print(f"Plotting directories created. Saving new plots")
+
+    ### plotting ###
+    for i in range(c.num_vars):
+        if i == 0:
+            name = 'rho'
+        elif i == 1:
+            name = 'u_x'
+        elif i == 2:
+            name = 'u_y'
+        elif i == 3:
+            name = 'u_z'
+        elif i == 4:
+            name = 'B_x'
+        elif i == 5:
+            name = 'B_y'
+        elif i == 6:
+            name = 'energy'
+        elif i == 7:
+            name = 'pressure'
+        
+        # isolate variable array
+        arr = meshOBJ[stagger_switch, i, :]
+        # full individualized path
+        img_path = plot_dir +'/' + name + '/tL' + str(tL) + '.png'
+        # plot
+        plt.plot(arr)
+        plt.xlabel("position")
+        plt.ylabel(name)
+        plt.title('t = ' + str(tL))
+        plt.grid()
+        print(img_path)
+        plt.savefig(img_path)
+        plt.close()
+
+    # rho_arr = meshOBJ[stagger_switch, 0, :]
+    # u_x_arr = meshOBJ[stagger_switch, 1, :] / rho_arr
+    # u_y_arr = meshOBJ[stagger_switch, 2, :] / rho_arr
+    # u_z_arr = meshOBJ[stagger_switch, 3, :] / rho_arr
+    # B_y_arr = meshOBJ[stagger_switch, 4, :] 
+    # B_z_arr = meshOBJ[stagger_switch, 5, :]
+    # en__arr = meshOBJ[stagger_switch, 6, :]
---
+++ 04-03-neb/mhd-0.py
@@ -0,0 +1,102 @@
+import numpy as np
+import math as m
+
+from scipy import constants
+import matplotlib.pyplot as plt
+import os
+
+from Library import minmod, calc_f, calc_f_cell, animate
+import configuration as c
+from datetime import datetime
+
+########## Variable Indices ###########
+# 0: rho 
+# 1: rho u_x 
+# 2: rho u_y 
+# 3: rho u_z 
+# 4: By 
+# 5: Bz 
+# 6: energy 
+
+########## Initial Parameters ##########
+# get the current time for plotting
+now =  datetime.now()
+
+# initialize stagger_switch
+stagger_switch = 0
+
+# construct initial grid
+meshOBJ = c.initialize(c.name)
+meshOBJ[1,:,:] = meshOBJ[0,:,:].copy()
+
+# to force the (data)typing with numpy to work
+update_arr = np.empty(c.num_vars)
+wjnhalf_arr = np.empty_like(update_arr)
+wjp1nhalf_arr = np.empty_like(update_arr)
+
+while c.t <= c.Tmax:
+
+    # iterate over entire array storing each variable component on every grid node: calculates and returns the derivative values at each cell fpr all vars
+    f_vals = calc_f(meshOBJ[stagger_switch, :, :], c.Bx, c.gamma)
+    
+    # loop over all cells, with a margin of 2 inside the boundaries
+    # leaving these two cells untouched should act as ghost cells, ensuring our boundaries stay "pinned" to the initial values (i.e., dirichlet condition)
+    for i in range(2,c.nx-2):
+        # loop calculations over each quantity in the supervector [rho, rho u_x, rho u_y, rho u_z, By, Bz, energy] since we vectorized the derivative funcs 
+        for equation_idx in range(c.num_vars):
+            # get 1D array of each scalar quantity (or component, i.e., By individually) on the domain
+            w = meshOBJ[stagger_switch, equation_idx, :]
+            # grab the argument of the divergence term in each MHD eqn by slicing the array returned from calc_f() (i.e., equation 4.2 in Balbas)
+            f_ = f_vals[equation_idx,:]
+            # use minmod to calculate spatial derivatives, following equation depending on whether offset mesh 
+            wp = minmod(c.alpha,w[i-1],w[i],w[i+1])
+            wpnp1 = minmod(c.alpha, w[i-(2*stagger_switch)], w[i-(2*stagger_switch)+1], w[i-(2*stagger_switch)+2])
+            # equation 2.19 in Balbas: i.e., the "predictor" step of the leapfrog (still switching between staggered mesh)
+            wjnhalf_arr[equation_idx] = w[i] - (c.lam/2.) * minmod(c.alpha, f_[i-1], f_[i], f_[i+1])
+            wjp1nhalf_arr[equation_idx] = w[i-(2*stagger_switch) + 1] - (c.lam/2.) * minmod(c.alpha, f_[i-(2*stagger_switch)], f_[i-(2*stagger_switch)+1], f_[i-(2*stagger_switch)+2])
+            # equation 2.17 (RHS) in Balbas: use the staggered cells to integrate exactly, in terms of our polynomial reconstruction
+            update_arr[equation_idx] = (1./2.) * (w[i] + w[i-(2*stagger_switch)+1]) + (-1 * stagger_switch)*(wp - wpnp1)/8.
+
+        # handle the temporal integral now (equation written on bottom of pg 266) 
+        # evaluate the derivatives of the quantities along the midcells using the predictor step, wjnhalf & wjp1nhalf
+        fjnhalf = calc_f_cell(wjnhalf_arr, c.Bx, c.gamma)
+        fjnp1half = calc_f_cell(wjp1nhalf_arr, c.Bx, c.gamma)
+
+        # handle the staggered mesh by using the plus/minus 
+        if stagger_switch == 0:
+            meshOBJ[1,:,i] = update_arr + c.lam*(fjnp1half - fjnhalf)
+        elif stagger_switch == 1:
+            meshOBJ[0,:,i] = update_arr - c.lam*(fjnp1half - fjnhalf)
+        else:
+            raise ValueError('The variable "stagger_switch" must be either zero or unity!')
+    
+    ### ARE WE UPDATING THE VELOCITIES BY DIVIDING BY RHO ELSEWHERE
+    # handle u_x reconstruction
+    rho_arr = rho_arr = meshOBJ[stagger_switch, 0, :]
+    meshOBJ[stagger_switch, 1, :] /= rho_arr
+    meshOBJ[stagger_switch, 2, :] /= rho_arr
+    meshOBJ[stagger_switch, 3, :] /= rho_arr
+
+    # benchmarking output 
+    # should largely incorporate the animation and plotting routines we have already made
+    if c.tL > 0:
+        if (c.tL % (c.nt // 10)) == 0:
+            percent = round((c.t / c.Tmax)*100.,2)
+            print('time t=',c.t)
+            print(r'progress is ', percent, r'% done')
+            # output data
+            # might need to call at first index: 1 - stagger_switch*1 to get the values that we JUST UPDATED IN THIS TIMESTEP,
+            # i.e., the values on the offset mesh (since we switch up and down between the j+1/2 and j each dt)
+
+            animate(meshOBJ, stagger_switch, c.tL, now)
+
+    if stagger_switch == 1:
+        stagger_switch = 0
+    elif stagger_switch == 0:
+        stagger_switch = 1
+    else:
+        raise ValueError('The variable "stagger_switch" must be either zero or unity!')
+
+    # advance time
+    c.t += c.dt
+    c.tL += 1
