7e41e0c0c34eead2df95e7431a251e4ac68a75f5
Kenneth Thompson <thompsoniiikenneth@gmail.com>
2025-04-08 00:10:38 -0400

    sigmoid and alpha runs fully coded and parallel. working on getting data for graphs still. uncomment code in run_me. obliterated the config file

---
+++ 04-07-CJ/Library.py
@@ -0,0 +1,125 @@
+import numpy as np
+import math as m
+
+from scipy import constants
+import matplotlib.pyplot as plt
+import configuration as c
+import os
+
+
+# minmod (see equation 2.8 and following discussion in Balbas et al)
+def minmod(alpha, wjm, wj, wjp):
+    if np.sign(wjm) == np.sign(wj) and np.sign(wj) == np.sign(wjp):
+        delminus = alpha * (wj - wjm)
+        delplus = alpha * (wjp - wj)
+        del0 = (1./2.) * (delminus + delplus)
+        # calculate minimums in the least efficient and most verbose way possible
+        dels = np.array([delminus, delplus, del0])
+        
+        return np.sign(wj) * np.nanmin(dels)
+    
+    else:
+        return 0.
+
+# calculate f() for all quantities in a single vector (following Balbas explicitly)
+def calc_f(meshOBJ, Bx, gam):
+    fs = np.zeros_like(meshOBJ)
+    for i in range(meshOBJ.shape[1]):
+        fs[:,i] = calc_f_cell(meshOBJ[:,i], Bx, gam)
+    return fs
+
+# function to actually encode the MHD equations: calculating the argument of the divergence at each cell
+def calc_f_cell(cell, Bx, gam):
+    # unpack the values of u vector (see equation 4.1 in balbas)
+    rho, rho_ux, rho_uy, rho_uz, By, Bz, e = cell
+    # equivalent of our old "reconstruct" routine
+    ux = rho_ux / rho
+    uy = rho_uy / rho
+    uz = rho_uz / rho
+    B2 = Bx**2 + By**2 + Bz**2
+    v2 = ux**2 + uy**2 + uz**2
+    
+    # Energy equation
+    p = (gam-1.) * (e - ((1./2.) * rho * v2) - (1./2.) * B2)
+    pstar = p + (1./2.) * B2
+    
+    # calculate the equations of MHD in a vectorized manner
+    # following the expression blindly from Balbas itself, instead of worrying about units and physics
+    # see equation (4.2, 4.3)
+    f0 = rho_ux
+    f1 = (rho_ux**2 / rho) + pstar - Bx**2
+    f2 = (rho_ux * uy) - Bx * By
+    f3 = (rho_ux * uz) - Bx * Bz
+    f4 = (By * ux) - Bx * uy
+    f5 = (Bz * ux) - Bx * uz
+    f6 = (e + pstar) * ux - Bx * np.dot(np.array([Bx,By,Bz]),np.array([ux,uy,uz]))
+    
+    return np.array([f0, f1, f2, f3, f4, f5, f6])
+
+# plotting function
+def animate(mesh, stagger_switch, tL, now, alpha, Tmax, nx, dt, dx, num_vars,name, sig=0): #  Tmax, nx, dt, dx
+    """saves individual plots for each variable"""
+
+    meshOBJ = mesh.copy()
+    # commented out for now so I dont get loaded with files
+    plot_dir = 'Plots--a=' + str(alpha) + '_Tmax=' + str(Tmax) + '_nx=' + str(nx) + '_dt=' + str(dt) + '_dx=' + str(dx)
+    if name == 'sigmoid': plot_dir += '_sig_' + str(sig)
+    # check if directories exist
+    if os.path.exists(plot_dir):
+        print(f"Saving new plots")
+    else:
+        os.mkdir(plot_dir)
+        os.mkdir(plot_dir+"/rho")
+        os.mkdir(plot_dir+"/u_x")
+        os.mkdir(plot_dir+"/u_y")
+        os.mkdir(plot_dir+"/u_z")
+        os.mkdir(plot_dir+"/B_x")
+        os.mkdir(plot_dir+"/B_y")
+        os.mkdir(plot_dir+"/energy")
+        os.mkdir(plot_dir+"/pressure")
+        print(f"Plotting directories created. Saving new plots")
+
+    ### plotting ###
+    for i in range(num_vars):
+        if i == 0:
+            name = 'rho'
+        elif i == 1:
+            name = 'u_x'
+            meshOBJ[stagger_switch, 1, :] /= meshOBJ[stagger_switch, 0, :] # divide by rho
+        elif i == 2:
+            name = 'u_y'
+            meshOBJ[stagger_switch, 2, :] /= meshOBJ[stagger_switch, 0, :] # divide by rho
+        elif i == 3:
+            name = 'u_z'
+            meshOBJ[stagger_switch, 3, :] /= meshOBJ[stagger_switch, 0, :] # divide by rho
+        elif i == 4:
+            name = 'B_x'
+        elif i == 5:
+            name = 'B_y'
+        elif i == 6:
+            name = 'energy'
+        elif i == 7:
+            name = 'pressure'
+
+        # isolate variable array
+        arr = meshOBJ[stagger_switch, i, :]
+        # full individualized path
+        img_path = plot_dir +'/' + name + '/tL' + str(tL) + '.png'
+        # plot
+        plt.plot(arr)
+        plt.xlabel("position")
+        plt.ylabel(name)
+        plt.title('t = ' + str(tL))
+        plt.grid()
+        print(img_path)
+        plt.savefig(img_path)
+        plt.close()
+
+    # rho_arr = meshOBJ[stagger_switch, 0, :]
+    # u_x_arr = meshOBJ[stagger_switch, 1, :] / rho_arr # does this do this twice ie. line 87?
+    # u_y_arr = meshOBJ[stagger_switch, 2, :] / rho_arr
+    # u_z_arr = meshOBJ[stagger_switch, 3, :] / rho_arr
+    # B_y_arr = meshOBJ[stagger_switch, 4, :]
+    # B_z_arr = meshOBJ[stagger_switch, 5, :]
+    # en_arr = meshOBJ[stagger_switch, 6, :]
+    # return rho_arr, u_x_arr, u_y_arr, u_z_arr, B_y_arr, B_z_arr, en_arr
---
+++ 04-07-CJ/run_me.py
@@ -0,0 +1,143 @@
+import numpy as np
+import math as m
+from scipy import constants
+import matplotlib.pyplot as plt
+import os
+import mhd as Balbas
+from datetime import datetime
+from joblib import Parallel, delayed
+from functools import partial
+
+
+
+def initialize(name, num_vars, nx, gamma, sigmoid_value=0.0):  # meshOBJ, alpha, Tmax, t, nt, tL, num_vars, Bx, gamma, nx, lam, n_plots
+    """returns the initial conditions based on the type of problem specified by the 'name' variable"""
+    # create object to store most recent iterations for all variables
+    meshOBJ = np.empty((2, num_vars, nx))
+
+    # choose the appropriate initial conditions based on the problem type
+    match name:
+        case 'sigmoid':
+            def sig(x): return 1 / (1 + np.exp(-sigmoid_value * x))
+            p0 = sig(np.linspace(-1, 1, nx))
+            rho = p0.copy()
+            en = p0 / (gamma - 1) + 1./2. # I think the 1/2 is the additional terms
+            plt.plot(np.linspace(-1, 1, nx), en)
+            meshOBJ[0, 0, :] = rho ; meshOBJ[0, -1, :] = en
+            meshOBJ[0, 1:-1, 0:(nx // 2)] = np.array([[ 0.0, 0.0, 0.0, 1.0, 0.0]]).T
+            meshOBJ[0, 1:-1, (nx // 2):nx] = np.array([[0.0, 0.0, 0.0, -1.0, 0.0]]).T  # WHY DIVIDE BY 10?
+        case 'Dai & Woodward':
+            pass
+            ######### THIS CASE REQUIRES BX TO BE CHANGING SO MAYBE NOT ##############
+            # ro0_neg = 1.08; pr0_neg = 0.95
+            # ux0 = 1.2; uy0 = 0.01; uz0 = 0.5
+            # bx0 = 2. / np.sqrt(16 * np.arctan(1.)); by0 = 3.6/ np.sqrt(16 * np.arctan(1.)); bz0 = 2. / np.sqrt(16 * np.arctan(1.))
+            # b_vec_neg = np.array([bx0, by0, bz0])
+            # u_vec_neg = np.array([ux0, uy0, uz0])
+
+            # ro0_pos = 1.; pr0_pos = 1
+            # ux0 = 0.; uy0 = 0.; uz0 = 0.
+            # bx0 = 2. / np.sqrt(16 * np.arctan(1.)); by0 = 4. / np.sqrt(16 * np.arctan(1.)); bz0 = 2. / np.sqrt(16 * np.arctan(1.))
+            # b_vec_pos = np.array([bx0, by0, bz0])
+            # u_vec_pos = np.array([ux0, uy0, uz0])
+            # meshOBJ[0, :, 0:(nx // 2)] = np.array([[1.08, 1.2 / 1.08, 0.01 / 1.08, 0.5 / 1.08, 1.0, 0.0, e0]]).T
+            # meshOBJ[0, :, (nx // 2):nx] = np.array([[0.125, 0.0, 0.0, 0.0, -1.0, 0.0, e0 / 10.]]).T
+
+        case 'Brio & Wu':
+
+            e0 = (1. / (gamma - 1.)) - (1. / 2.) # ASK ABOUT THIS. WHERE DO WE INCORPERATE PRESSURE TO IC?
+
+            meshOBJ[0, :, 0:(nx // 2)] = np.array([[1.0, 0.0, 0.0, 0.0, 1.0, 0.0, e0]]).T
+            meshOBJ[0, :, (nx // 2):nx] = np.array([[0.125, 0.0, 0.0, 0.0, -1.0, 0.0, e0 / 10.]]).T # WHY DIVIDE BY 10?
+
+            # ro0_neg = 1.; pr0_neg = 1.
+            # ux0 = 0.; uy0 = 0.; uz0 = 0.
+            # bx0 = .75; by0 = 1.; bz0 = 0.
+            # b_vec_neg = np.array([bx0, by0, bz0])
+            # u_vec_neg = np.array([ux0, uy0, uz0])
+
+            # ro0_pos = .125; pr0_pos = .1
+            # ux0 = 0.; uy0 = 0.; uz0 = 0.
+            # bx0 = .75; by0 = -1.; bz0 = 0
+            # b_vec_pos = np.array([bx0, by0, bz0])
+            # u_vec_pos = np.array([ux0, uy0, uz0])
+
+        case 'slow shock':
+            pass
+            # ro0_neg = 1.368; pr0_neg = 1.769
+            # ux0 = 0.269; uy0 = 1.0; uz0 = 0.
+            # bx0 = 1.; by0 = 0.; bz0 = 0.
+            # b_vec_neg = np.array([bx0, by0, bz0])
+            # u_vec_neg = np.array([ux0, uy0, uz0])
+
+            # ro0_pos = 1.; pr0_pos = 1.
+            # ux0 = 0.; uy0 = 0.; uz0 = 0.
+            # bx0 = 1.; by0 = 1.; bz0 = 0
+            # b_vec_pos = np.array([bx0, by0, bz0])
+            # u_vec_pos = np.array([ux0, uy0, uz0])
+            e0 = (1. / (gamma - 1.)) - (1. / 2.)
+            # B_X must also be 1.0 not .75
+            meshOBJ[0, :, 0:(nx // 2)] = np.array([[1.368, 0.269 * 1.368, 1.0 * 1.368, 0.0 , 0.0, 0.0, e0]]).T
+            meshOBJ[0, :, (nx // 2):nx] = np.array([[1.0, 0.0, 0.0, 0.0, 1.0, 0.0, e0 / 10.]]).T
+
+        case 'rarefaction':
+            pass
+            # ro0_neg = 1.; pr0_neg = 2.
+            # ux0 = 0.; uy0 = 0.; uz0 = 0.
+            # bx0 = 1.; by0 = 0.; bz0 = 0.
+            # b_vec_neg = np.array([bx0, by0, bz0])
+            # u_vec_neg = np.array([ux0, uy0, uz0])
+
+            # ro0_pos = .2; pr0_pos = 0.1368
+            # ux0 = 1.186; uy0 = 2.967; uz0 = 0.
+            # bx0 = 1.; by0 = 1.6405; bz0 = 0
+            # b_vec_pos = np.array([bx0, by0, bz0])
+            # u_vec_pos = np.array([ux0, uy0, uz0])
+            e0 = (1. / (gamma - 1.)) - (1. / 2.)
+            meshOBJ[0, :, 0:(nx // 2)] = np.array([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, e0]]).T
+            meshOBJ[0, :, (nx // 2):nx] = np.array([[0.2, 1.186 * 0.2, 2.967 * 0.2, 0.0, 1.6405, 0.0, e0 / 10.]]).T
+        case _:
+            raise ValueError(
+                'invalid testing: use ""Dai & Woodward"" or  ""Brio & Wu"" or ""slow shock"" or ""rarefaction""')
+
+    return meshOBJ
+
+# name='Brio & Wu'
+# nx = 200
+#################################### Individual Alpha Run ####################################
+# meshOBJ= initialize(name=name, num_vars=7, nx=nx, gamma=2)
+# Balbas.balbas_one_dimension(meshOBJ, alpha=1.4, Tmax=.2, num_vars=7, Bx=.75, gamma=2, nx=nx, n_plots=20, CFL_safety=40, length=1, name=name, alpha_test=True)
+#################################### Variational Alpha Runs ####################################
+# fixed inputs for alpha variation runs
+# os.mkdir(f'alpha_test_dir_case_{name}')
+# meshOBJ= initialize(name=name, num_vars=7, nx=nx, gamma=2)
+# fixed_work_alpha_run = partial(Balbas.balbas_one_dimension,
+#      meshOBJ=meshOBJ, Tmax=.2, num_vars=7, Bx=.75, gamma=2, nx=nx, n_plots=5, CFL_safety=40, length=2, name=name, alpha_test=True)
+# # alpha_vals = np.arange(.8, 4, .4)
+# alpha_vals = [1, 1.4]
+#
+# # this is the parrallel run for all of the alpha values // -1 means it runs on all available cores set to whatever you actually want it to be
+# Parallel(n_jobs=-1)(delayed(fixed_work_alpha_run)(alpha=a) for a in alpha_vals)
+
+# plotting alpha graphs (working in the testing.ipynb file)
+name = 'sigmoid'
+nx = 200
+#################################### Individual Sigmoid Run ####################################
+# meshOBJ= initialize(name=name, num_vars=7, nx=nx, gamma=2, sigmoid_value=10)
+# Balbas.balbas_one_dimension(meshOBJ, alpha=1.4, Tmax=.2, num_vars=7, Bx=.75, gamma=2, nx=nx, n_plots=10, CFL_safety=40, length=2, name=name, alpha_test=False, sigmoid_test=True)
+#################################### Sigmoid IC Runs ####################################
+sigmoid_vals = np.arange(1, 50, 10);mesh_inputs = []
+for sig in sigmoid_vals: mesh_inputs.append(initialize(name=name, num_vars=7, nx=nx, gamma=2, sigmoid_value=sig))
+os.mkdir(f'sigmoid_test')
+fixed_work_sigmoid_run = partial(Balbas.balbas_one_dimension,
+     alpha=1.4, Tmax=.2, num_vars=7, Bx=.75, gamma=2, nx=nx, n_plots=5, CFL_safety=40, length=2, name=name, alpha_test=True)
+
+args = list(zip(mesh_inputs, sigmoid_vals))
+Parallel(n_jobs=-1)(delayed(fixed_work_sigmoid_run)(meshOBJ=mesh, sig=sig) for mesh, sig in args)
+
+
+
+
+
+
+
